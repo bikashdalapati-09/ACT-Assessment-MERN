a4dd5b8e1375a0f4b28643a6bc7348e151b45f6b
bikashdalapati-09 <bikashdalapati09@gmail.com>
2026-03-26 20:20:34 +0530

    database connection completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,18 @@
-import app from './app.js';
-import dotenv from 'dotenv'
+import app from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "../db/index.js";
 
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);   
+connectDB()
+.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+})
+.catch(() => {
+    console.log("Database Connection error");
+    
 })
